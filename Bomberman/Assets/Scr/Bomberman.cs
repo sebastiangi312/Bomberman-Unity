4e3e064f3df7352e1a86df60bb222cce287a9eaa
CLoaizaF <cloaiza@unal.edu.co>
2022-05-20 21:05:14 -0500

    Add movement to bomberman script #T008

--- Bomberman/Assets/Scr/Bomberman.cs
+++ Bomberman/Assets/Scr/Bomberman.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Bomberman : MonoBehaviour
 {
+
+    [SerializeField]
+    private float speed = 2;
+    [SerializeField]
+    private Camera _cam;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,13 @@ public class Bomberman : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector3(horizontal, vertical);
+        dir.Normalize();
+        Vector3 pos = transform.position;
+        pos +=  dir.y * speed * Time.deltaTime * transform.up; //X = V * T
+        pos +=  dir.x * speed * Time.deltaTime * transform.right;
+        transform.position = pos;
     }
 }
