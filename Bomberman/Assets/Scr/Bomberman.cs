9de0f10cafab9baf27d1d52a229243b73d2c2711
Danny-19 <danyestrada1999@gmail.com>
2022-05-26 23:51:23 -0500

    Animaciones Bomberman

--- Bomberman/Assets/Scr/Bomberman.cs
+++ Bomberman/Assets/Scr/Bomberman.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class Bomberman : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    private float speed = 2;
+    [SerializeField]
+
+    private Vector2 _velocity;
+
+    [SerializeField]
+    private Rigidbody2D _rb;
+
+    [SerializeField]
+    private GameObject _bombPrefab;
+
+    [SerializeField]
+    private Transform _bombPoint;
     void Start()
     {
         
@@ -13,6 +27,29 @@ public class Bomberman : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 _dir  = new Vector2(horizontal, vertical);
+        _dir.Normalize();
+        _velocity = speed * _dir;
         
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            
+            GameObject projectile = Instantiate(_bombPrefab);
+            projectile.transform.position = _bombPoint.position;
+            projectile.transform.rotation = _bombPoint.rotation;
+            
+        }
+    }
+
+    private void FixedUpdate()
+    {
+     
+        _rb.velocity = _velocity;
+       
     }
 }
