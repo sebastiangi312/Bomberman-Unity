d42298db2c93a9c1593be93fa87ae75a4231d0f8
CLoaizaF <cloaiza@unal.edu.co>
2022-06-16 19:24:13 -0500

    Add path finding #T011

--- Bomberman/Assets/Scr/Enemy/Enemy.cs
+++ Bomberman/Assets/Scr/Enemy/Enemy.cs
@@ -5,26 +5,43 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [Space(20)]
-
-    [SerializeField]
-    private EnemyConfig _enemyConfig;
     
     private PathFindingController _pathFindingController;
-    private StateMachineController _stateMachineController;
-    
-    public EnemyConfig EnemyConfig => _enemyConfig;
-    public PathFindingController PathFindingController => _pathFindingController;
-    public StateMachineController StateMachineController => _stateMachineController;
+
+    [SerializeField]
+    private List<Transform> _pathPoints;
+
+    private Vector3 _nextPoint;
+    private int _currentPointIndex = 0;
     
     public void Start()
     {
         _pathFindingController = GetComponent<PathFindingController>();
-        _stateMachineController = new StateMachineController();
-        _stateMachineController.Init(this);      
+        _nextPoint = GetEnemyNextPoint();
+        Debug.Log(_nextPoint);
+        this._pathFindingController.GoTo(_nextPoint);
     }
     
     void Update()
     {
-        _stateMachineController.OnUpdate();
+        float distance = (_nextPoint - transform.position).magnitude;
+
+        if(distance <= 0.01f) 
+        {
+            _nextPoint = GetEnemyNextPoint();
+            this._pathFindingController.GoTo(_nextPoint);
+        }
+        
+    }
+
+    private Vector3 GetEnemyNextPoint()
+    {
+        if (this._pathPoints == null || this._pathPoints.Count == 0)
+        {
+            return Vector3.zero;
+        }
+        
+        _currentPointIndex = (_currentPointIndex + 1) % this._pathPoints.Count;
+        return this._pathPoints[_currentPointIndex].position;
     }
 }
